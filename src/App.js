78745c676ad1c277ae368ee8d458d2cea71bbf93
Gabriel200107 <rm86675@fiap.com.br>
2021-09-26 20:48:16 -0300

    site pronto

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 import Home from "./components/home/Home";
 import Menu from "./components/menu/Menu";
 import './App.css'
-import Rodape from "./components/rodape/Rodape";
+import Footer from "./components/footer/Footer";
 
 export default function App() {
 
    return (
       <>
         <Menu/>
-        <Rodape/>
+        <Footer/>
       </>
    )
 }
